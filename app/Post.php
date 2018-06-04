b3627a149515677e17629c295b58749d5d1ff178
yoshizawa <yoshizawa@2ndwave.jp>
2018-06-04 04:55:20 +0000

    laravel tutorial

---
+++ app/Post.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    //
+    protected $table = 'posts';
+    /**
+     * 複数代入する属性
+     *
+     * @var array
+     */
+    protected $fillable = ['title', 'content'];
+}
---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        //postsテーブルをtruncate
+        DB::table('posts')->truncate();
+        //ダミーデータを生成
+        factory(App\Post::class, 10)->create();
+    }
+}
